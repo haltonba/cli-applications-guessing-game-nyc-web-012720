697368f51d1d44450c5178a833a56a272014732f
Halton Bagley <haltonba94@gmail.com>
2020-01-17 19:24:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   number = rand(6) + 1
   puts "Guess a number between 1 and 6"
   input = gets.chomp
-  if number == input
+  if number == input.to_i
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
